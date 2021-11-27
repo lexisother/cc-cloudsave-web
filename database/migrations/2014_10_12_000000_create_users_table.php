f9470caffdb178156805c49019768420bab77536
Alyxia Sother <lexisoth2005@gmail.com>
2021-11-27 19:51:50 +0100

    [database] Add API tokens to users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('api_token', 60)->unique()->nullable();
+            $table->rememberToken();
             $table->mediumText('savefile')->nullable();
             $table->timestamps();
         });
