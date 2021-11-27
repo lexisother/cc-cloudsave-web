196154047ad0bd58f39507082a5dcf76ce3bc4c5
Alyxia Sother <lexisoth2005@gmail.com>
2021-11-27 19:53:20 +0100

    [routes/api] Add a registration route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post("/register", [App\Http\Controllers\Auth\RegisterController::class, 'register']);
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
